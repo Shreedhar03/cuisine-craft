0037f2e5f4e66276cc27e5783852f7337bc24d09
Shreedhar Deodatta Urawane <urawane03@gmail.com>
2024-05-28 00:13:53 +0530

    partial home page

--- index.php
+++ index.php
@@ -8,13 +8,22 @@
 
 <body>
 
-    <main class="bg-teal-900 h-screen flex flex-col justify-center">
+    <main class="bg-teal-900 h-[75vh] flex flex-col justify-center">
 
-        <div class="flex flex-col items-center">
+        <div class="flex flex-col items-center gap-3">
             <h1 class="logo text-white text-5xl text-center">CuisineCraft</h1>
-            <div class="flex items-center gap-3">
-                <a href="login.php" class="px-4 py-2 rounded-lg bg-slate-200 text-teal-800 font-semibold">Login</a>
-                <a href="signup.php" class="px-4 py-2 rounded-lg bg-slate-200 text-teal-800 font-semibold">Sign Up</a>
+
+            <h3 class="font-bold text-3xl text-white">
+                Effortless. Modern. Intuitive.
+            </h3>
+
+            <p class="text-xl text-white text-center mb-4">
+                <!-- Manage your restaurant menus effortlessly<br /> with our intuitive platform.<br /> -->
+                Get your digital menu up and running quickly with our intuitive platform
+            </p>
+            <div class="flex items-center gap-2">
+                <a href="login.php" class="px-6 py-2 rounded-lg bg-slate-200 text-lg text-teal-800 font-semibold">Login</a>
+                <a href="signup.php" class="px-6 py-2 rounded-lg border border-white text-lg text-white font-semibold">Sign Up</a>
             </div>
         </div>
 
